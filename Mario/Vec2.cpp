fa529c9a9ed868375d052c506a9b0466524624de
Ngoc Chinh <ngocchinhlqd@gmail.com>
2020-09-22 06:48:24 +0700

    create source tree

--- Mario/Vec2.cpp
+++ Mario/Vec2.cpp
@@ -1,1 +1,38 @@
 #include "Vec2.h"
+
+void Vec2::SetX(float x)
+{
+	this->x = x;
+}
+
+float Vec2::GetX()
+{
+	return this->x;
+}
+
+void Vec2::SetY(float y)
+{
+	this->y = y;
+}
+
+float Vec2::GetY()
+{
+	return this->y;
+}
+
+float Vec2::ToAbsoluteValue()
+{
+	return sqrt(pow(this->x, 2) + pow(this->y, 2));
+}
+
+void Vec2::SetAbsoluteValue(float value)
+{
+	float _Scale = value / this->ToAbsoluteValue();
+	this->SetX(this->GetX() * _Scale);
+	this->SetY(this->GetY() * _Scale);
+}
+
+Vec2 Vec2::operator+(const Vec2&)
+{
+	return Vec2();
+}
--- Mario/Vec2.h
+++ Mario/Vec2.h
@@ -1,5 +1,39 @@
 #pragma once
+#include <math.h>
+
+using namespace std;
+
 class Vec2
 {
+private:
+	float x;
+	float y;
+public:
+	Vec2() {
+		this->x = 0;
+		this->y = 0;
+	}
+
+	Vec2(float x, float y) {
+		this->x = x;
+		this->y = y;
+	}
+
+	void SetX(float x);
+
+	float GetX();
+
+	void SetY(float y);
+
+	float GetY();
+
+	float ToAbsoluteValue();
+
+	void SetAbsoluteValue(float value);
+
+	Vec2 operator+(const Vec2&);
+	Vec2 operator-(const Vec2&);
+	Vec2 operator*(const Vec2&);
+	Vec2 operator/(const Vec2&);
 };
 
