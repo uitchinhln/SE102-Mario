c842765ba1c12da08b420f1cf552ee09c70e1572
Ngoc Chinh <ngocchinhlqd@gmail.com>
2020-10-03 17:45:34 +0700

    add temp folder

---
+++ Temp/Test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="1" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="870" columns="29">
+ <image source="../Resource/Tileset/1.png" trans="ff80c0" width="498" height="516"/>
+</tileset>
---
+++ Temp/Test3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="5" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="1872" columns="39">
+ <image source="../Resource/Tileset/5.gif" trans="0080ff" width="678" height="830"/>
+</tileset>
