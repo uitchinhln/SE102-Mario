72faeb50f126d3f95cf62900da661797982068ce
Ngoc Chinh <ngocchinhlqd@gmail.com>
2020-11-06 16:56:37 +0700

    [Remove] unused resource files

--- Mario/Resource/Xml/OldMap/Maps/Tileset-1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Tileset" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="870" columns="29">
- <image source="1.png" trans="ff80c0" width="498" height="516"/>
-</tileset>
--- Mario/Resource/Xml/TilesetToMap/TilesetToMap6.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="TilesetToMap6" tilewidth="32" tileheight="32" tilecount="1056" columns="32">
- <image source="TilesetToMap2.png" width="1024" height="1056"/>
-</tileset>
